cf02883e6da4ed51e7e9c48b0668ff4c0c059d6f
swiftcode <mpc@junct.com>
2021-09-16 21:11:34 -0500

    Project setup
    Add gitignore
    Remove storyboard and references to Main
    Setup SceneDelegate
    
    Changes to be committed:
    	new file:   .gitignore
    	modified:   Pool.xcodeproj/project.pbxproj
    	deleted:    Pool/Base.lproj/Main.storyboard
    	modified:   Pool/Info.plist
    	modified:   Pool/SceneDelegate.swift
    	modified:   Pool/ViewController.swift

--- Pool/SceneDelegate.swift
+++ Pool/SceneDelegate.swift
@@ -10,13 +10,21 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    var navigationController: UINavigationController?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        let rootViewController = ViewController()
+        navigationController = UINavigationController()
+        navigationController?.viewControllers = [rootViewController]
+        navigationController?.navigationBar.barTintColor = .systemGray6
+        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.black]
+
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {

a28fb0ca4b75b8d863c3b2ac36d4ea5e40a66a3a
swiftcode <mpc@junct.com>
2021-09-19 03:05:37 -0500

    Teams model
    Add json file for Teams data and create the data model
    
    Changes to be committed:
    	modified:   Pool.xcodeproj/project.pbxproj
    	new file:   Pool/Models/Team.swift
    	new file:   Pool/Teams/teams.json

---
+++ Pool/Models/Team.swift
@@ -0,0 +1,29 @@
+//
+//  Team.swift
+//  Pool
+//
+//  Created by mpc on 9/19/21.
+//
+
+import Foundation
+
+struct Team: Codable {
+    let city, name, abr: String
+    let conf: Conf
+    let div: Div
+}
+
+enum Conf: String, Codable {
+    case afc = "AFC"
+    case nfc = "NFC"
+}
+
+enum Div: String, Codable {
+    case divEast = "East"
+    case east = "EAST"
+    case north = "North"
+    case south = "South"
+    case west = "West"
+}
+
+typealias Teams = [Team]

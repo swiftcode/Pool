d1d97658bd863c05fab79f45de54937fe711722d
JaredP45 <jpaubel@pm.me>
2021-10-18 21:11:34 -0500

    WIP Created TableViewController and TableViewCell files for DataEntry.  On branch DataEntryTable  Changes to be committed: 	modified:   Pool.xcodeproj/project.pbxproj 	new file:   Pool/DataEntry/DataEntryTableViewCell.swift 	new file:   Pool/DataEntry/DataEntryTableViewController.swift

---
+++ Pool/DataEntry/DataEntryTableViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  DataEntryTableViewCell.swift
+//  Pool
+//
+//  Created by Jared Paubel on 10/18/21.
+//
+
+import UIKit
+
+class DataEntryTableViewCell: UITableViewCell {
+
+    var teamName: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    
+    var weeklyCode: UIView = {
+        
+    }()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    
+
+}

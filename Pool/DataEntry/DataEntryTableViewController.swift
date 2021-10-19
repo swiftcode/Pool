9ee5f95e73884d1a01ba80163fac6949dceb48b4
JaredP45 <jpaubel@pm.me>
2021-10-19 09:48:26 -0500

    Updated table view with minor changes.  On branch DataEntryTable  Your branch is up to date with 'origin/DataEntryTable'.
     Changes to be committed:
    	modified:   Pool/DataEntry/DataEntryTableViewController.swift

--- Pool/DataEntry/DataEntryTableViewController.swift
+++ Pool/DataEntry/DataEntryTableViewController.swift
@@ -7,39 +7,35 @@
 
 import UIKit
 
-class DataEntryTableViewController: UITableViewController {
+class DataEntryTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     //MARK: - Data
     let dummyNums = ["Week1", "Week2"]
-
+    
+    let tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        view.addSubview(tableView)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
+    func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
     }
 
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath) as! DataEntryßTableViewCell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath) as! DataEntryTableViewCell
 
-        // Configure the cell...
-
+        cell.textLabel?.text = "Cell \(indexPath.row + 1)"
         return cell
     }
 

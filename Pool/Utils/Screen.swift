77e3bd762f6e99f78e3686187c536b1df654f03e
Collinear <dbtrump@gmail.com>
2021-10-24 22:11:23 -0400

    Create Screen.swift
    Introspection on screen dimensions.

---
+++ Pool/Utils/Screen.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+struct Screen {
+    static let screenSize = UIScreen.main.bounds
+    static let width = screenSize.width
+    static let height = screenSize.height
+    static let scale = UIScreen.main.scale
+    static let nativeBounds = UIScreen.main.nativeBounds
+}

4017d97aa358fc3c9951ad0691a3595ea1212254
Late To IT <65265786+lateTWoIT@users.noreply.github.com>
2021-11-01 18:44:37 -0500

    Parsing the JSON File
    Teams Parsing

--- Pool/Home/HomeViewController.swift
+++ Pool/Home/HomeViewController.swift
@@ -14,5 +14,31 @@ class HomeViewController: UIViewController {
 
         view.backgroundColor = .white
         self.title = "Home"
+        
+        parseJSON()
+    }
+    
+    private func parseJSON() {
+        guard let path = Bundle.main.path(forResource: "teams", ofType: "json") else {
+            return
+        }
+        let url = URL(fileURLWithPath: path)
+        var result: TeamData?
+        do {
+            let jsonData = try Data(contentsOf: url)
+            result = try JSONDecoder().decode(TeamData.self, from: jsonData)
+            
+            if let result = result {
+                print(result)
+            }
+            else {
+                print("Failed to parse")
+            }
+            return
+        }
+        catch {
+            print("Error: \(error)")
+        }
     }
 }
+
--- Pool/Models/Team.swift
+++ Pool/Models/Team.swift
@@ -7,23 +7,35 @@
 
 import Foundation
 
-struct Team: Codable {
-    let city, name, abr: String
-    let conf: Conf
-    let div: Div
+//struct Team: Codable {
+//    let city, name, abr: String
+//    let conf: Conf
+//    let div: Div
+//}
+//
+//enum Conf: String, Codable {
+//    case afc = "AFC"
+//    case nfc = "NFC"
+//}
+//
+//enum Div: String, Codable {
+//      case divEast = "East"
+//    case east = "East"
+//    case north = "North"
+//    case south = "South"
+//    case west = "West"
+//}
+//
+//typealias Teams = [Team]
+
+struct TeamData: Codable {
+    let data: [ResultItem]
 }
 
-enum Conf: String, Codable {
-    case afc = "AFC"
-    case nfc = "NFC"
+struct ResultItem: Codable {
+    let city: String
+    let name: String
+    let abr: String
+    let conf: String
+    let div: String
 }
-
-enum Div: String, Codable {
-    case divEast = "East"
-    case east = "EAST"
-    case north = "North"
-    case south = "South"
-    case west = "West"
-}
-
-typealias Teams = [Team]

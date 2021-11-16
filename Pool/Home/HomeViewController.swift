4f769df2a1bb89f70ee58c4e0bf723646b3ab7a7
Late To IT <65265786+lateTWoIT@users.noreply.github.com>
2021-11-15 18:53:53 -0600

    JSON Parse in progress

--- Pool/Home/HomeViewController.swift
+++ Pool/Home/HomeViewController.swift
@@ -9,36 +9,17 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    var teams: [TeamData] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .white
         self.title = "Home"
         
-        parseJSON()
+        ParseJSON()
     }
     
-    private func parseJSON() {
-        guard let path = Bundle.main.path(forResource: "teams", ofType: "json") else {
-            return
-        }
-        let url = URL(fileURLWithPath: path)
-        var result: TeamData?
-        do {
-            let jsonData = try Data(contentsOf: url)
-            result = try JSONDecoder().decode(TeamData.self, from: jsonData)
-            
-            if let result = result {
-                print(result)
-            }
-            else {
-                print("Failed to parse")
-            }
-            return
-        }
-        catch {
-            print("Error: \(error)")
-        }
-    }
+
 }
 

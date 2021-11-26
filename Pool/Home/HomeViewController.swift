80ca1a508bd5e954d5519643b4bbc5c8a008765b
swiftcode <mpc@junct.com>
2021-11-26 00:54:06 -0600

    Home Screen
    Stub in temporary images for the home screen
    Add image to the home screen
    
    Changes to be committed:
    	modified:   Pool.xcodeproj/project.pbxproj
    	modified:   Pool/Home/HomeView.swift
    	modified:   Pool/Home/HomeViewController.swift
    	modified:   Pool/Scores/ScoresView.swift
    	new file:   Pool/sports.jpg
    	new file:   Pool/sports2.jpg

--- Pool/Home/HomeViewController.swift
+++ Pool/Home/HomeViewController.swift
@@ -9,10 +9,17 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    var homeView: HomeView = {
+        let view = HomeView(frame: .zero)
+        return view
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .white
-        self.title = "Home"
+        view.addSubview(homeView)
+
+        homeView.addConstraint(topAnchor: view.topAnchor, leadingAnchor: view.leadingAnchor, trailingAnchor: view.trailingAnchor, bottomAnchor: view.bottomAnchor, paddingTop: 0.0, paddingLeft: 0.0, paddingRight: 0.0, paddingBottom: 0.0, width: 0.0, height: 0.0)
     }
 }

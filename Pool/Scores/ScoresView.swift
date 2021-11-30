87e251fd64d4a047b7199843b8f705c93ca071cf
Late To IT <65265786+lateTWoIT@users.noreply.github.com>
2021-11-29 20:54:13 -0600

    Update ScoresView.swift
    Fix merge conflict - deleted lines.

--- Pool/Scores/ScoresView.swift
+++ Pool/Scores/ScoresView.swift
@@ -8,27 +8,13 @@
 import UIKit
 
 class ScoresView: UIView {
-
-//<<<<<<< Updated upstream
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-=======
-    //var welcome = Welcome()
-  
+ 
     //MARK: - Initialization
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
-        //welcome.get("https://google.com")
   }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
->>>>>>> Stashed changes
     }
-    */
-
 }
